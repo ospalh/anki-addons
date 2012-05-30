2243d93c00b0636cca4e4301b08b0ff69e569166
ospalh <ospalh@gmail.com>
2012-05-30 17:53:45 +0200

    bump version

---
+++ dehashilator/__init__.py
@@ -0,0 +1,9 @@
+# -*- mode: python ; coding: utf-8 -*-
+# Copyright © 2012 Roland Sieker
+# This file: License: GNU GPL, version 3 or later;
+# http://www.gnu.org/copyleft/gpl.html
+#
+# Caveat Emptor. Different files are under different
+# licences. Especially progress.py is CC-by-nc.
+
+__version__ = '0.0a2'
