a0b33f055f889496d6d9cb8fc461ab1330f659e5
ospalh <ospalh@gmail.com>
2012-05-31 11:09:06 +0200

    Fixes for the class style

--- dehashilate.py
+++ dehashilate.py
@@ -3,13 +3,22 @@
 # This file: License: GNU GPL, version 3 or later;
 # http://www.gnu.org/copyleft/gpl.html
 #
-# Rename files that have MD5ish names with names derived from the note
-# content.
-#
 
-from dehashilator.dehashilator import dehashilate
+"""Change the names of files to more readable versions.
+
+Go through the collection and detect files that alook like MD5
+hashes used by Anki <1.2, look at the note for a better name and
+rename the files, changing the notes as well.
+
+"""
+
+from dehashilator import Dehashilator
 from aqt import mw
 from aqt.qt import *
+
+def dehashilate():
+    dehashilator = Dehashilator()
+    dehashilator.dehashilate()
 
 dhma = QAction(mw)
 dhma.setText("Dehashilate media")
