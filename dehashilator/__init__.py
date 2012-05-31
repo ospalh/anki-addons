6afea6842fb2e3794aa3934b2616e71ecb489265
ospalh <ospalh@gmail.com>
2012-05-31 11:07:23 +0200

    Classified. Also there’s other stuff.

--- dehashilator/__init__.py
+++ dehashilator/__init__.py
@@ -6,4 +6,18 @@
 # Caveat Emptor. Different files are under different
 # licences. Especially progress.py is CC-by-nc.
 
+"""Change the names of files to more readable versions.
+
+Go through an Anki 2 collection and detect files that alook like MD5
+hashes used by Anki <1.2, look at the note for a better name and
+rename the files, changing the notes as well. This module is rather
+useless without Anki 2.
+
+"""
+
+from dehashilator import Dehashilator
+from progress import progress
+from romaji import *
+
 __version__ = '0.0a2'
+__all__ = ['Dehashilator', 'progress', 'roma', 'html', 'kana']
