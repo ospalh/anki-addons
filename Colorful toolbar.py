7cfd788074b4da7358c5ea48055b094e31a4fbff
ospalh <ospalh@gmail.com>
2012-07-13 13:23:57 +0200

    Looks like it works reasonably well.

--- Colorful toolbar.py
+++ Colorful toolbar.py
@@ -3,11 +3,13 @@
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 # Images:
 # most icons from Anki1
-# Exceptions: study.png,
+# Exceptions: 
+# study.png,
 # Found at http://www.vectorarts.net/vector-icons/free-study-book-icons/ ,
 # "Free for Personal and Commercial Use"
 #
-# The 'bury' and suspend icon were found at openclipart.org:
+# A few others, notably the 'bury', 'suspend', 'options', 'record' and
+# 'play recorded' icons were found at openclipart.org:
 # Free: http://creativecommons.org/publicdomain/zero/1.0/
 
 
@@ -54,7 +56,6 @@ def go_edit_current():
     except AttributeError:
         pass
 
-
 def go_edit_layout():
     """Edit the current card's note's layout if there is one."""
     try:
@@ -64,18 +65,21 @@ def go_edit_layout():
         return
 
 def toggle_text_tool_bar():
+    """Switch the original toolbar on or off."""
     if show_text_tool_bar_action.isChecked():
         mw.toolbar.web.show()
     else:
         mw.toolbar.web.hide()
 
 def toggle_qt_tool_bar():
+    """Switch the new upper tool bar on or off."""
     if show_qt_tool_bar_action.isChecked():
         mw.qt_tool_bar.show()
     else:
         mw.qt_tool_bar.hide()
 
 def toggle_more_tool_bar():
+    """Switch the new lower tool bar on or off."""
     # No real need to check if we are in review. Only then should we
     # be able to activate the action.
     if show_more_tool_bar_action.isChecked():
@@ -84,141 +88,9 @@ def toggle_more_tool_bar():
         mw.reviewer.more_tool_bar.hide()
         
 def ask_delete():
+    """Delete a note after asking the user."""
     if askUser('Delete note?', defaultno=True):
         mw.reviewer.onDelete()
-
-# Make all the actions top level, so we can use them for the menu and
-# the tool bar.
-
-# Most of these icons are part of the standard version, but as they
-# are not currently used by the standard version, they may disappear
-# when dae gets around to doing some clean up. So bring them along,
-# anyway.
-sync_action = QAction(mw)
-sync_action.setText("S&ync")
-sync_action.setIcon(QIcon(os.path.join(icons_dir, 'sync.png')))
-mw.connect(sync_action, SIGNAL("triggered()"), mw.onSync)
-decks_action = QAction(mw)
-decks_action.setText("&Deck browser")
-decks_action.setIcon(QIcon(os.path.join(icons_dir, 'deck_browser.png')))
-mw.connect(decks_action, SIGNAL("triggered()"), go_deck_browse)
-overview_action = QAction(mw)
-overview_action.setText("Deck overview")
-overview_action.setIcon(QIcon(os.path.join(icons_dir, 'study_options.png')))
-mw.connect(overview_action, SIGNAL("triggered()"), mw.onOverview)
-study_action = QAction(mw)
-study_action.setText("Study")
-study_action.setIcon(QIcon(os.path.join(icons_dir, 'study.png')))
-mw.connect(study_action, SIGNAL("triggered()"), go_study)
-add_notes_action = QAction(mw)
-add_notes_action.setText("Add notes")
-add_notes_action.setIcon(QIcon(os.path.join(icons_dir, 'add.png')))
-mw.connect(add_notes_action, SIGNAL("triggered()"), mw.onAddCard)
-browse_cards_action = QAction(mw)
-browse_cards_action.setText("Browse cards")
-browse_cards_action.setIcon(QIcon(os.path.join(icons_dir, 'browse.png')))
-mw.connect(browse_cards_action, SIGNAL("triggered()"), mw.onBrowse)
-statistics_action = QAction(mw)
-statistics_action.setText("Show statistics")
-statistics_action.setIcon(QIcon(os.path.join(icons_dir, 'statistics.png')))
-mw.connect(statistics_action, SIGNAL("triggered()"), mw.onStats)
-edit_current_action = QAction(mw)
-edit_current_action.setText("Edit current")
-edit_current_action.setIcon(QIcon(os.path.join(icons_dir, 'edit_current.png')))
-mw.connect(edit_current_action, SIGNAL("triggered()"), go_edit_current)
-edit_layout_action = QAction(mw)
-edit_layout_action.setText("Edit layout")
-edit_layout_action.setIcon(QIcon(os.path.join(icons_dir, 'edit_layout.png')))
-mw.connect(edit_layout_action, SIGNAL("triggered()"), go_edit_layout)
-
-
-## Template to add actions:
-# NN_action = QAction(mw)
-# NN_action.setText("Show NN")
-# NN_action.setIcon(QIcon(os.path.join(icons_dir, 'NN.png')))
-# mw.connect(NN_action, SIGNAL("triggered()"), mw.onNN)
-
-
-## Actions to show and hide the different tool bars.
-
-show_text_tool_bar_action = QAction(mw)
-show_text_tool_bar_action.setText("Show text tool bar")
-show_text_tool_bar_action.setCheckable(True)
-#show_text_tool_bar_action.setIcon(QIcon(os.path.join(icons_dir, 'browse.png')))
-mw.connect(show_text_tool_bar_action, SIGNAL("triggered()"), toggle_text_tool_bar)
-
-show_qt_tool_bar_action = QAction(mw)
-show_qt_tool_bar_action.setText("Show icon bar")
-show_qt_tool_bar_action.setCheckable(True)
-show_qt_tool_bar_action.setChecked(True)
-#show_qt_tool_bar_action.setIcon(QIcon(os.path.join(icons_dir, 'browse.png')))
-mw.connect(show_qt_tool_bar_action, SIGNAL("triggered()"), toggle_qt_tool_bar)
-
-show_more_tool_bar_action = QAction(mw)
-show_more_tool_bar_action.setText("Show more tool bar")
-show_more_tool_bar_action.setCheckable(True)
-show_more_tool_bar_action.setChecked(True)
-show_more_tool_bar_action.setEnabled(False)
-#show_more_tool_bar_action.setIcon(QIcon(os.path.join(icons_dir, 'browse.png')))
-mw.connect(show_more_tool_bar_action, SIGNAL("triggered()"), toggle_more_tool_bar)
-
-toggle_mark_action = QAction(mw)
-toggle_mark_action.setText("Mark")
-toggle_mark_action.setCheckable(True)
-toggle_mark_icon = QIcon()
-# Something's rotten.
-#toggle_mark_icon.addFile(fileName=os.path.join(icons_dir, 'mark_off.png'),
-#                         state=QIcon.Off)
-#toggle_mark_icon.addFile(fileName=os.path.join(icons_dir, 'mark_on.png'),
-#                         state=QIcon.On)
-# """TypeError: QIcon.addFile(QString, QSize size=QSize(), QIcon.Mode
-# mode=QIcon.Normal, QIcon.State state=QIcon.Off): 'state' is not a
-# valid keyword argument"""
-# What?
-# Off is default.
-toggle_mark_icon.addFile(os.path.join(icons_dir, 'mark_off.png'))
-# Provide all arguments in order. No point in guessing what the name of
-# state really is.
-toggle_mark_icon.addFile(os.path.join(icons_dir, 'mark_on.png'), QSize(),
-                         QIcon.Normal, QIcon.On)
-toggle_mark_action.setIcon(toggle_mark_icon)
-mw.connect(toggle_mark_action, SIGNAL("triggered()"), mw.reviewer.onMark)
-bury_action = QAction(mw)
-bury_action.setText("Bury note")
-bury_action.setIcon(QIcon(os.path.join(icons_dir, 'bury.png')))
-mw.connect(bury_action, SIGNAL("triggered()"), mw.reviewer.onBuryNote)
-suspend_action = QAction(mw)
-suspend_action.setText("Suspend note")
-suspend_action.setIcon(QIcon(os.path.join(icons_dir, 'suspend.png')))
-mw.connect(suspend_action, SIGNAL("triggered()"), mw.reviewer.onSuspend)
-delete_action = QAction(mw)
-delete_action.setText("Delete note")
-delete_action.setIcon(QIcon(os.path.join(icons_dir, 'delete.png')))
-mw.connect(delete_action, SIGNAL("triggered()"), ask_delete)
-
-
-
-## Add images to actions we already have. I skip a few where no icon
-## really fits.
-
-mw.form.actionDocumentation.setIcon(QIcon(os.path.join(icons_dir, 'help.png')))
-mw.form.actionDonate.setIcon(QIcon(os.path.join(icons_dir, 'donate.png')))
-mw.form.actionAbout.setIcon(QIcon(os.path.join(icons_dir, 'anki.png')))
-mw.form.actionUndo.setIcon(QIcon(os.path.join(icons_dir, 'undo.png')))
-mw.form.actionSwitchProfile.setIcon(QIcon(os.path.join(icons_dir, 'user-head.png')))
-mw.form.actionImport.setIcon(QIcon(os.path.join(icons_dir, 'import.png')))
-mw.form.actionExport.setIcon(QIcon(os.path.join(icons_dir, 'export.png')))
-mw.form.actionExit.setIcon(QIcon(os.path.join(icons_dir, 'exit.png')))
-mw.form.actionDownloadSharedPlugin.setIcon(
-    QIcon(os.path.join(icons_dir, 'download-addon.png')))
-mw.form.actionFullDatabaseCheck.setIcon(
-    QIcon(os.path.join(icons_dir, 'check-db.png')))
-mw.form.actionPreferences.setIcon(QIcon(os.path.join(icons_dir, 'preferences.png')))
-
-
-
-## Template for adding images:
-# mw.form.actionNn.setIcon(QIcon(os.path.join(icons_dir, 'nn.png')))
 
 
 def add_tool_bar():
@@ -250,20 +122,23 @@ border-bottom: 1px solid #aaa;
         mw.mainLayout.insertWidget(1, mw.qt_tool_bar)
     # Add the actions here
     mw.qt_tool_bar.addAction(sync_action)
-    mw.qt_tool_bar.addAction(edit_current_action)
+    # Put this in the more tool bar, closer to the old edit button
+    #    mw.qt_tool_bar.addAction(edit_current_action)
     mw.qt_tool_bar.addAction(decks_action)
     mw.qt_tool_bar.addAction(overview_action)
     mw.qt_tool_bar.addAction(study_action)
     mw.qt_tool_bar.addAction(add_notes_action)
     mw.qt_tool_bar.addAction(browse_cards_action)
     mw.qt_tool_bar.addAction(statistics_action)
-    #mw.qt_tool_bar.addAction(_action)
-
-
 
 
 def add_more_tool_bar():
-    # This realy belongs to the reviewer. TODO
+    """
+    Add a tool bar at the bottom.
+
+    This provieds colorful command buttons for the functions usually
+    hidden in the "More" button at the bottom.
+    """
     try:
         mw.reviewer.more_tool_bar = QToolBar()
     except AttributeError:
@@ -283,10 +158,17 @@ border-bottom: 1px solid #aaa;
     # Todo: get index of the bottom web button thingy.
     mw.mainLayout.insertWidget(2, mw.reviewer.more_tool_bar)
     # Add the actions here
+    mw.reviewer.more_tool_bar.addAction(edit_current_action)
     mw.reviewer.more_tool_bar.addAction(toggle_mark_action)
     mw.reviewer.more_tool_bar.addAction(bury_action)
     mw.reviewer.more_tool_bar.addAction(suspend_action)
     mw.reviewer.more_tool_bar.addAction(delete_action)
+    mw.reviewer.more_tool_bar.addSeparator()
+    mw.reviewer.more_tool_bar.addAction(options_action)
+    mw.reviewer.more_tool_bar.addSeparator()
+    mw.reviewer.more_tool_bar.addAction(replay_action)
+    mw.reviewer.more_tool_bar.addAction(record_own_action)
+    mw.reviewer.more_tool_bar.addAction(replay_own_action)
     more_tool_bar_off()
 
 
@@ -303,13 +185,13 @@ def add_to_menus():
     # Add sync to the file memu. It was there in Anki 1.
     mw.form.menuCol.insertAction(mw.form.actionImport, sync_action)
     # Make a new top level menu and insert it.
-    view_menu = QMenu("&View", mw)
+    view_menu = QMenu(u"&View", mw)
     mw.form.menubar.insertMenu(mw.form.menuTools.menuAction() , view_menu)
     view_menu.addAction(show_qt_tool_bar_action)
     view_menu.addAction(show_text_tool_bar_action)
     view_menu.addAction(show_more_tool_bar_action)
     # And another one
-    go_menu = QMenu("&Go", mw)
+    go_menu = QMenu(u"&Go", mw)
     mw.form.menubar.insertMenu(mw.form.menuTools.menuAction() , go_menu)
     # Add DSAB to the new go menu
     go_menu.addAction(decks_action)
@@ -344,7 +226,6 @@ def edit_actions_off():
     except AttributeError:
         pass
 
-
 def edit_actions_on():
     """Switch on the edit actions."""
     try:
@@ -354,6 +235,7 @@ def edit_actions_on():
         pass
 
 def more_tool_bar_off():
+    """Hide the more tool bar."""
     show_more_tool_bar_action.setEnabled(False)
     bury_action.setEnabled(False)
     toggle_mark_action.setEnabled(False)
@@ -366,6 +248,7 @@ def more_tool_bar_off():
 
 
 def maybe_more_tool_bar_on():
+    """Show the more tool bar when we should."""
     show_more_tool_bar_action.setEnabled(True)
     bury_action.setEnabled(True)
     toggle_mark_action.setEnabled(True)
@@ -380,17 +263,18 @@ def maybe_more_tool_bar_on():
 
 
 def save_toolbars_visible():
-    mw.pm.profile['ctb_show_toolbar'] = show_text_tool_bar_action.isChecked()
-    mw.pm.profile['ctb_show_qt_toolbar'] = show_qt_tool_bar_action.isChecked()
-    mw.pm.profile['ctb_show_more_toolbar'] = show_more_tool_bar_action.isChecked()
-
-
-def save_toolbars_visible():
+    """Save if we should show the tool bars in the profile."""
     mw.pm.profile['ctb_show_toolbar'] = show_text_tool_bar_action.isChecked()
     mw.pm.profile['ctb_show_qt_toolbar'] = show_qt_tool_bar_action.isChecked()
     mw.pm.profile['ctb_show_more_toolbar'] = show_more_tool_bar_action.isChecked()
 
 def  load_toolbars_visible():
+    """
+    Show the right tool bars.
+
+    Get the state if we should show the tool bars from the profile or
+    use default values. Then show or do not show those tool bars.
+    """
     try:
         ttb_on = mw.pm.profile['ctb_show_toolbar']
     except KeyError:
@@ -412,9 +296,150 @@ def  load_toolbars_visible():
     # deck browser screen
     # toggle_more_tool_bar()
 
-
 def update_mark_action():
+    """Set the state of the mark action to the marked state of the note."""
     toggle_mark_action.setChecked(mw.reviewer.card.note().hasTag("marked"))
+
+
+
+# Make all the actions top level, so we can use them for the menu and
+# the tool bar.
+
+# Most of the icons are part of the standard version, but as they are
+# not currently used by the standard version, they may disappear when
+# dae gets around to doing some clean up. So bring them along, anyway.
+sync_action = QAction(mw)
+sync_action.setText(u"S&ync")
+sync_action.setIcon(QIcon(os.path.join(icons_dir, 'sync.png')))
+sync_action.setToolTip(u"Synchronize with AnkiWeb.")
+mw.connect(sync_action, SIGNAL("triggered()"), mw.onSync)
+decks_action = QAction(mw)
+decks_action.setText(u"&Deck browser")
+decks_action.setIcon(QIcon(os.path.join(icons_dir, 'deck_browser.png')))
+decks_action.setToolTip(u"Go to the deck browser.")
+mw.connect(decks_action, SIGNAL("triggered()"), go_deck_browse)
+overview_action = QAction(mw)
+overview_action.setText(u"Deck overview")
+overview_action.setIcon(QIcon(os.path.join(icons_dir, 'study_options.png')))
+overview_action.setToolTip(u"Go to the deck overview.")
+mw.connect(overview_action, SIGNAL("triggered()"), mw.onOverview)
+study_action = QAction(mw)
+study_action.setText(u"Study")
+study_action.setIcon(QIcon(os.path.join(icons_dir, 'study.png')))
+study_action.setToolTip(u"Start studying the selected deck.")
+mw.connect(study_action, SIGNAL("triggered()"), go_study)
+add_notes_action = QAction(mw)
+add_notes_action.setText(u"Add notes")
+add_notes_action.setIcon(QIcon(os.path.join(icons_dir, 'add.png')))
+add_notes_action.setToolTip(u"Add notes.")
+mw.connect(add_notes_action, SIGNAL("triggered()"), mw.onAddCard)
+browse_cards_action = QAction(mw)
+browse_cards_action.setText(u"Browse cards")
+browse_cards_action.setIcon(QIcon(os.path.join(icons_dir, 'browse.png')))
+browse_cards_action.setToolTip(u"Open the cards browser.")
+mw.connect(browse_cards_action, SIGNAL("triggered()"), mw.onBrowse)
+statistics_action = QAction(mw)
+statistics_action.setText(u"Show statistics")
+statistics_action.setIcon(QIcon(os.path.join(icons_dir, 'statistics.png')))
+statistics_action.setToolTip(u"Show statistics.")
+mw.connect(statistics_action, SIGNAL("triggered()"), mw.onStats)
+edit_current_action = QAction(mw)
+edit_current_action.setText(u"Edit current")
+edit_current_action.setIcon(QIcon(os.path.join(icons_dir, 'edit_current.png')))
+edit_current_action.setToolTip(u"Edit the current note.")
+mw.connect(edit_current_action, SIGNAL("triggered()"), go_edit_current)
+edit_layout_action = QAction(mw)
+edit_layout_action.setText(u"Edit layout")
+edit_layout_action.setIcon(QIcon(os.path.join(icons_dir, 'edit_layout.png')))
+edit_layout_action.setToolTip(u"Edit the layout of the current card.")
+mw.connect(edit_layout_action, SIGNAL("triggered()"), go_edit_layout)
+toggle_mark_action = QAction(mw)
+toggle_mark_action.setText(u"Mark")
+toggle_mark_action.setCheckable(True)
+toggle_mark_action.setToolTip(u"Mark or unmark the current note.")
+toggle_mark_icon = QIcon()
+toggle_mark_icon.addFile(os.path.join(icons_dir, 'mark_off.png'))
+toggle_mark_icon.addFile(os.path.join(icons_dir, 'mark_on.png'), QSize(),
+                         QIcon.Normal, QIcon.On)
+toggle_mark_action.setIcon(toggle_mark_icon)
+mw.connect(toggle_mark_action, SIGNAL("triggered()"), mw.reviewer.onMark)
+bury_action = QAction(mw)
+bury_action.setText(u"Bury note")
+bury_action.setIcon(QIcon(os.path.join(icons_dir, 'bury.png')))
+bury_action.setToolTip(u"Hide this note until the deck is closed.")
+mw.connect(bury_action, SIGNAL("triggered()"), mw.reviewer.onBuryNote)
+suspend_action = QAction(mw)
+suspend_action.setText(u"Suspend note")
+suspend_action.setIcon(QIcon(os.path.join(icons_dir, 'suspend.png')))
+suspend_action.setToolTip(u"Hide this note permanently.")
+mw.connect(suspend_action, SIGNAL("triggered()"), mw.reviewer.onSuspend)
+delete_action = QAction(mw)
+delete_action.setText(u"Delete note")
+delete_action.setIcon(QIcon(os.path.join(icons_dir, 'delete.png')))
+delete_action.setToolTip(u"Delete this note.")
+mw.connect(delete_action, SIGNAL("triggered()"), ask_delete)
+options_action = QAction(mw)
+options_action.setText(u"Study options")
+options_action.setIcon(QIcon(os.path.join(icons_dir, 'options.png')))
+options_action.setToolTip(u"Show the active study options group.")
+mw.connect(options_action, SIGNAL("triggered()"), mw.reviewer.onOptions)
+replay_action = QAction(mw)
+replay_action.setText(u"Replay audio")
+replay_action.setIcon(QIcon(os.path.join(icons_dir, 'replay.png')))
+replay_action.setToolTip(u"Replay card’s audio or video.")
+mw.connect(replay_action, SIGNAL("triggered()"), mw.reviewer.replayAudio)
+record_own_action = QAction(mw)
+record_own_action.setText(u"Record own voice")
+record_own_action.setIcon(QIcon(os.path.join(icons_dir, 'record_own.png')))
+record_own_action.setToolTip(u"Record your own voice.")
+mw.connect(record_own_action, SIGNAL("triggered()"), mw.reviewer.onRecordVoice)
+replay_own_action = QAction(mw)
+replay_own_action.setText(u"Replay own voice")
+replay_own_action.setIcon(QIcon(os.path.join(icons_dir, 'replay_own.png')))
+replay_own_action.setToolTip(u"Replay your recorded voice.")
+mw.connect(replay_own_action, SIGNAL("triggered()"),
+           mw.reviewer.onReplayRecorded)
+
+## Actions to show and hide the different tool bars.
+show_text_tool_bar_action = QAction(mw)
+show_text_tool_bar_action.setText(u"Show text tool bar")
+show_text_tool_bar_action.setCheckable(True)
+mw.connect(show_text_tool_bar_action, SIGNAL("triggered()"),
+           toggle_text_tool_bar)
+show_qt_tool_bar_action = QAction(mw)
+show_qt_tool_bar_action.setText(u"Show icon bar")
+show_qt_tool_bar_action.setCheckable(True)
+show_qt_tool_bar_action.setChecked(True)
+mw.connect(show_qt_tool_bar_action, SIGNAL("triggered()"), toggle_qt_tool_bar)
+show_more_tool_bar_action = QAction(mw)
+show_more_tool_bar_action.setText(u"Show more tool bar")
+show_more_tool_bar_action.setCheckable(True)
+show_more_tool_bar_action.setChecked(True)
+show_more_tool_bar_action.setEnabled(False)
+mw.connect(show_more_tool_bar_action, SIGNAL("triggered()"),
+           toggle_more_tool_bar)
+
+
+## Add images to actions we already have. I skip a few where no icon
+## really fits.
+mw.form.actionDocumentation.setIcon(QIcon(os.path.join(icons_dir, 'help.png')))
+mw.form.actionDonate.setIcon(QIcon(os.path.join(icons_dir, 'donate.png')))
+mw.form.actionAbout.setIcon(QIcon(os.path.join(icons_dir, 'anki.png')))
+mw.form.actionUndo.setIcon(QIcon(os.path.join(icons_dir, 'undo.png')))
+mw.form.actionSwitchProfile.setIcon(QIcon(os.path.join(icons_dir, 'user-head.png')))
+mw.form.actionImport.setIcon(QIcon(os.path.join(icons_dir, 'import.png')))
+mw.form.actionExport.setIcon(QIcon(os.path.join(icons_dir, 'export.png')))
+mw.form.actionExit.setIcon(QIcon(os.path.join(icons_dir, 'exit.png')))
+mw.form.actionDownloadSharedPlugin.setIcon(
+    QIcon(os.path.join(icons_dir, 'download-addon.png')))
+mw.form.actionFullDatabaseCheck.setIcon(
+    QIcon(os.path.join(icons_dir, 'check-db.png')))
+mw.form.actionPreferences.setIcon(QIcon(os.path.join(icons_dir, 'preferences.png')))
+
+## Hide the edit and nmore buttons.
+mw.reviewer._bottomCSS += ".stat {display:none;}"
+
+
 
 
 # Create the menus
