310d5a00e1f5aab72689a810598e01c78a481058
ospalh <ospalh@gmail.com>
2012-07-27 11:19:27 +0200

    Bump version

--- dehashilator/__init__.py
+++ dehashilator/__init__.py
@@ -20,6 +20,6 @@ from progress import progress
 from exists import exists_lc
 from romaji import *
 
-__version__ = '1.0.0b4'
+__version__ = '1.0.0b5'
 __all__ = ['test_and_dehashilate', 'progress', 'roma', 'html', 'kana',
            'exists_lc', '__version__']
