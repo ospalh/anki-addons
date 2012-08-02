c501e92a5d85a66e130ed52a54044c5b9740582f
ospalh <ospalh@gmail.com>
2012-08-02 11:52:19 +0200

    PEP8-ified a bit (mostly just got rid of camelCase). Set factor to 1.5.

--- Scalar.py
+++ Scalar.py
@@ -11,27 +11,27 @@ import re
 
 # Code word to look for in the field name to decide whether to do the
 # number comparison:
-ScalarField = 'Scalar'
+scalar_field = 'Scalar'
 
 # Factor to decide what counts as ‘good enough’. It should be > 1.0
 # (or at least >= 1.0). What you use here depends on how precisely you
 # want to remember your numbers.
-passFactor = 2.0
+pass_factor = 1.5
 
 # How the number is coloured.
-failColour= '#f00'
-passColour= '#ff0'
-exactColour= '#0f0'
+fail_color= '#f00'
+pass_color= '#ff0'
+exact_color= '#0f0'
 # (It looks like ‘red’, ‘yellow’ and ‘green’ work but are
 # different colours.)
 
 
 
-def scalarTypeAnsAnswerFilter(self, buf):
+def scalar_type_ans_answer_filter(self, buf):
     # Redo bits of typeQuesAnswerFilter to get the field name typed in and most of the old typeAnsAnswerFilter.
     m = re.search(self.typeAnsPat, buf)
-    colourString = u''
-    scalarWorked = False
+    colour_string = u''
+    scalar_worked = False
     if not self.typeCorrect:
         return re.sub(self.typeAnsPat, "", buf)
     # tell webview to call us back with the input content
@@ -42,18 +42,26 @@ def scalarTypeAnsAnswerFilter(self, buf):
     # Cascade of tests
     if m:
         fld = m.group(1)
-        if not fld.startswith("cq:") and ScalarField in fld:
-            scalarWorked, colourString = scalarColour(cor, self.typedAnswer)
-    if scalarWorked:
+        if not fld.startswith("cq:") and scalar_field in fld:
+            scalar_worked, colour_string = scalar_color(cor, self.typedAnswer)
+    if scalar_worked:
         return re.sub(self.typeAnsPat, """
 <span id=coransscalar style="font-family: '%s'; font-size: %spx; colour: black; background: %s">%s</span>""" %
-                      (self.typeFont, self.typeSize, colourString, self.typedAnswer), buf)
+                      (self.typeFont, self.typeSize, colour_string, self.typedAnswer), buf)
     else:
-        return oldTypeAnsAnswerFilter(self, buf)
+        return old_type_ans_answer_filter(self, buf)
 
-def scalarColour(a, b):
-    """Return the colour the answer should be, red, yellow or green,
- depending on how close we are to the target."""
+def scalar_color(a, b):
+    """
+    Return a Boolean and a color string.
+
+    Return True and a string that can be used to color a html text. The color
+    is a red, yellow or green depending on how close the two numbers a
+    and b are to each other.
+
+    When a and b can't be converted to numbers, False and an empty
+    string is returend.
+    """
     try:
         target_value = int(a)
         given_value =  int(b)
@@ -66,16 +74,16 @@ def scalarColour(a, b):
     # One of the two conversions worked: we have two valid numbers, two
     # ints or two floats. We don’t really care which.
     if target_value == given_value:
-        return True, exactColour
+        return True, exact_color
     # Now we know that they are not the same, so either red or yellow.
     try:
         factor = 1.0 * given_value/target_value
     except ZeroDivisionError:
-        return True, failColour
-    if factor < 1.0/passFactor or factor > passFactor:
-        return True, failColour
-    return True, passColour
+        return True, fail_color
+    if factor < 1.0/pass_factor or factor > pass_factor:
+        return True, fail_color
+    return True, pass_color
 
 
-oldTypeAnsAnswerFilter = Reviewer.typeAnsAnswerFilter
-Reviewer.typeAnsAnswerFilter = scalarTypeAnsAnswerFilter
+old_type_ans_answer_filter = Reviewer.typeAnsAnswerFilter
+Reviewer.typeAnsAnswerFilter = scalar_type_ans_answer_filter
