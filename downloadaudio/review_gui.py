1efef1fd91532b879de9654e8cd874ec1ae14737
ospalh <ospalh@gmail.com>
2012-09-17 14:07:48 +0200

    The dialog looks good.

--- downloadaudio/review_gui.py
+++ downloadaudio/review_gui.py
@@ -22,9 +22,9 @@ choices what to do wit each:
 import os
 # normal
 # from aqt import mw
+# from anki.sounds import play
 
-
-
+# from blacklist import add_black_hash
 
 
 
@@ -37,28 +37,40 @@ from PyQt4.QtGui import *
 # icons_dir = os.path.join(mw.pm.addonFolder(), 'icons')
 icons_dir = os.path.join('.', 'icons')
 
+# to make the code a bit more readable
+action = {'add' : 0, 'keep': 1, 'delete': 2, 'blacklist': 3}
+
 # def store_or_blacklist(note, retrieved_data):
 def store_or_blacklist(note, retrieved_data):
     if not note or not retrieved_data:
         return
-    #for source, dest, text, dl_fname, dl_hash in retrieved_data:
+    #for (source, dest, text, dl_fname, dl_hash) in retrieved_data:
     #    pass
     review_files = ReviewFiles(note, retrieved_data)
     if not review_files.exec_():
         remove_all_files(retrieved_data)
         return
-    for rm_file in review_files.delete:
-        os.remove(rm_file)
-    for bl_hash in review_files.blacklist:
-        # blacklist(bl_hash)
+    # Go through the list once and just do what needs to be done.
+    # Keep track if we have to do some clean up.
+    items_added = False
+    for idx, (source, dest, text, dl_fname, dl_hash) \
+            in enumerate(retrieved_data):
+        action_id = review_files.buttons_groups[idx].checkedId()
+        print 'id for row ', idx, 'is ', action_id
+        if action_id == action['add']:
+            print 'add for list row ', idx
+            # Add dest field and file name pair to list
+            items_added = True
+            # note[dest] += '[sound:' + dl_fname + ']'
+        if action_id == action['delete']:
+            print 'remove for list row ', idx
+            # os.remove(os.path.join(mw.mediaDir(), dl_fname))
+        if action_id == action['blacklist']:
+            print 'blacklist for list row ', idx
+            # add_black_hash(dl_hash)
+    if items_added:
         pass
-    for dest, fname in review_files.add_to_note:
-        # note[dest] += '[sound:' + fname + ']'
-        pass
-    if review_files.add_to_note:
-        # Make sure data is stored permanently
-        # note.reset()
-        pass
+        # note.flush()
 
 
 def remove_all_files(files_etc):
@@ -71,10 +83,7 @@ class ReviewFiles(QDialog):
     def __init__(self, note, files_list):
         self.note = note
         self.list = files_list
-        self.buttonBox = None
-        self.add_to_note = []
-        self.delete = []
-        self.blacklist = []
+        # self.buttonBox = None
         self.buttons_groups = []
         super(ReviewFiles, self).__init__() # Voodoo code. Look it up!
         self.initUI()
@@ -108,6 +117,14 @@ class ReviewFiles(QDialog):
         rule_label = QLabel('<hr>')
         layout.addWidget(rule_label, 2, 0, 1, 7)
         self.create_rows(layout)
+        dialog_buttons = QDialogButtonBox(self)
+        dialog_buttons.addButton(QDialogButtonBox.Cancel)
+        dialog_buttons.addButton(QDialogButtonBox.Ok)
+        self.connect(dialog_buttons, SIGNAL("accepted()"),
+                     self, SLOT("accept()"))
+        self.connect(dialog_buttons, SIGNAL("rejected()"),
+                     self, SLOT("reject()"))
+        layout.addWidget(dialog_buttons, 5+ 3, 0, 1, 7)
 
 
     def create_rows(self, layout):
@@ -124,34 +141,34 @@ class ReviewFiles(QDialog):
 
             t_button_group = QButtonGroup(self)
             t_button_group.setExclusive(True)
+            # Now the four buttons
             t_add_button = QPushButton(self)
             t_add_button.setCheckable(True)
             t_add_button.setChecked(True)
             t_add_button.setFlat(True)
-            # test
             t_add_button.setIcon(QIcon(os.path.join(icons_dir, 'add.png')))
             layout.addWidget(t_add_button, num, 3)
-            t_button_group.addButton(t_add_button, 0)
+            t_button_group.addButton(t_add_button, action['add'])
             t_keep_button = QPushButton(self)
             t_keep_button.setCheckable(True)
             t_keep_button.setFlat(True)
             t_keep_button.setIcon(QIcon(os.path.join(icons_dir, 'keep.png')))
             layout.addWidget(t_keep_button, num, 4)
-            t_button_group.addButton(t_keep_button, 1)
+            t_button_group.addButton(t_keep_button,  action['keep'])
             t_delete_button = QPushButton(self)
             t_delete_button.setCheckable(True)
             t_delete_button.setFlat(True)
             t_delete_button.setIcon(QIcon(os.path.join(icons_dir,
                                                        'delete.png')))
             layout.addWidget(t_delete_button, num, 5)
-            t_button_group.addButton(t_delete_button, 2)
+            t_button_group.addButton(t_delete_button,  action['delete'])
             t_blacklist_button = QPushButton(self)
             t_blacklist_button.setCheckable(True)
             t_blacklist_button.setFlat(True)
             t_blacklist_button.setIcon(QIcon(os.path.join(icons_dir,
                                                           'blacklist.png')))
             layout.addWidget(t_blacklist_button, num, 6)
-            t_button_group.addButton(t_blacklist_button, 3)
+            t_button_group.addButton(t_blacklist_button,  action['blacklist'])
             self.buttons_groups.append(t_button_group)
 
 
