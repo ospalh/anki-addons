701455898cce7158d5ce167dd4477ab8038b4761
ospalh <ospalh@gmail.com>
2012-09-27 12:53:01 +0200

    The edit-before-download works in principle.

--- downloadaudio/update_gui.py
+++ downloadaudio/update_gui.py
@@ -12,25 +12,37 @@ from language import default_audio_language_code
 Change the download audio parameters on user input.
 """
 
-def update_pairs(general_pairs, japanese_pairs, language_code):
+def update_data(general_fields, japanese_fields, language_code):
     """Return updated download information"""
-    review_fields = ReviewFields(general_pairs, japanese_pairs, language_code)
+    print 'general fields: ', general_fields
+    print 'japanese fields: ', japanese_fields
+    review_fields = ReviewFields(general_fields, japanese_fields,
+                                 language_code)
     if not review_fields.exec_():
         return [], [], None
+    for num, (source, dest, old_text) in enumerate(general_fields):
+        general_fields[num] = (source, dest,
+                               review_fields.general_text_lineedits[num].text())
+    for num, (source, dest, old_kanji, old_kana) in enumerate(japanese_fields):
+        japanese_fields[num] = (source, dest,
+                                review_fields.kanji_lineedits[num].text(),
+                                review_fields.kana_lineedits[num].text())
     language_code = review_fields.language_code_lineedit.text()
-    print language_code
-    return general_pairs, japanese_pairs, language_code
+    return general_fields, japanese_fields, language_code
 
 
 class ReviewFields(QDialog):
     """
     A Dialog to let the user edit the texts or change the language.
     """
-    def __init__(self, general_pairs, japanese_pairs, language_code):
-        self.general_pairs = general_pairs,
-        self.japanese_pairs = japanese_pairs
+    def __init__(self, general_fields, japanese_fields, language_code):
+        self.general_fields = general_fields
+        self.japanese_fields = japanese_fields
         self.language_code = language_code # possibly None
         self.language_code_lineedit = None
+        self.general_text_lineedits = []
+        self.kanji_lineedits = []
+        self.kana_lineedits = []
         super(ReviewFields, self).__init__() # Cut-and-pasted
         self.initUI()
 
@@ -41,7 +53,7 @@ class ReviewFields(QDialog):
         layout = QVBoxLayout()
         self.setLayout(layout)
         explanation = QLabel(self)
-        if len(self.general_pairs) + len(self.japanese_pairs) > 0:
+        if len(self.general_fields) + len(self.japanese_fields) > 0:
             explanation.setText(
                 u'Please edit the text below or change the language.')
         else :
@@ -49,7 +61,7 @@ class ReviewFields(QDialog):
         layout.addWidget(explanation)
         self.create_general_rows(layout)
         self.create_japanese_rows(layout)
-        lang_hlayout = QHBoxLayout(self)
+        lang_hlayout = QHBoxLayout()
         lc_label = QLabel(u'Language code:', self)
         lang_hlayout.addWidget(lc_label)
         self.language_code_lineedit = QLineEdit(self)
@@ -70,7 +82,27 @@ class ReviewFields(QDialog):
 
 
     def create_general_rows(self, layout):
-        pass
+        gf_layout = QGridLayout()
+        for num, (source, dest, text) in enumerate(self.general_fields):
+            print num, source, dest, text
+            label = QLabel(u'{0}:'.format(source))
+            gf_layout.addWidget(label, num, 0)
+            ledit = QLineEdit(text)
+            gf_layout.addWidget(ledit, num, 1)
+            self.general_text_lineedits.append(ledit)
+        layout.addLayout(gf_layout)
+
 
     def create_japanese_rows(self, layout):
-        pass
+        jf_layout = QGridLayout()
+        for num, (source, dest, kanji, kana) in enumerate(self.japanese_fields):
+            print num, source, dest, kanji, kana
+            label = QLabel(u'{0}:'.format(source))
+            jf_layout.addWidget(label, num, 0)
+            kanji_edit = QLineEdit(kanji)
+            jf_layout.addWidget(kanji_edit, num, 1)
+            self.kanji_lineedits.append(kanji_edit)
+            kana_edit = QLineEdit(kana)
+            jf_layout.addWidget(kana_edit, num, 2)
+            self.kana_lineedits.append(kana_edit)
+        layout.addLayout(jf_layout)
