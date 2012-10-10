4e89c7173f809fc5c426d14b37749fcd164296e1
ospalh <ospalh@gmail.com>
2012-10-10 14:16:46 +0200

    Use lxml, less buggy and sait to be quicker.

--- lean browser qa.py
+++ lean browser qa.py
@@ -4,20 +4,19 @@
 # Copyright © 2012 Roland Sieker, <ospalh@gmail.com>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-import re
-from BeautifulSoup import BeautifulSoup
+from lxml import html
 from aqt.browser import DataModel
 
 # Define the name of the class you want to be hidden in the browser.
-hide_class_name = u'browserhide'
+hide_class_selector = '.browserhide'
 
 
 def reduce_format_qa(self, text):
-    soup = BeautifulSoup(text)
-    for hide in soup.findAll(True,
-                        {'class': re.compile('\\b' + hide_class_name + '\\b')}):
-        hide.extract()
-    return old_format_qa(self, unicode(soup))
+    doc = html.fromstring(text)
+    for el in doc.cssselect(hide_class_selector):
+        el.drop_tree()
+    return old_format_qa(self,
+                         unicode(html.tostring(doc, encoding='utf-8'),'utf-8'))
 
 
 old_format_qa = DataModel.formatQA
