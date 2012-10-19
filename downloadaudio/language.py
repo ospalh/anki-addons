11bbdef6922a4ed6ee380b0418728a07b3c17638
ospalh <ospalh@gmail.com>
2012-10-19 14:12:41 +0200

    Rearranged comments.

--- downloadaudio/language.py
+++ downloadaudio/language.py
@@ -4,34 +4,39 @@
 # Copyright © 2012 Roland Sieker, <ospalh@gmail.com>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
+"""
+Return a language code.
+"""
+
 import re
 from aqt import mw
 
 
-## Set the two-letter language code for the language you are learning
-## here. (This is typically not your native language.)
 default_audio_language_code = "ja"
+"""
+Language code for the language you are learning here.
+
+This is the code for the language for the downloaded audio. It is
+typically not your native language. Change this if you are not
+learning Japanese.
+"""
 
 
 ### Dont't change this!
 al_code_code = 'addon_audio_download_language'
 
-"""
-Return a two-letter language code.
-"""
-
-
-# Brainstorm: mw.col.decks.current()['conf']
 
 def get_language_code(card=None):
+    """
+    Return a language code.
+    """
     if not card:
         return default_audio_language_code
     # First, look at the tags
     for tag in card.note().tags:
         try:
-            return re.search('^lang_([a-z]{2,3})$', tag, flags=re.IGNORECASE)\
-                .group(1).lower()
-
+            return re.search('^lang_([a-z]{2,3})$', tag,
+                             flags=re.IGNORECASE).group(1).lower()
         except:
             continue
     # Then, look at the deck conf
