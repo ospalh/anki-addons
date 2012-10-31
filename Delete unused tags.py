fe3bcc48a77425f09cb24ba8197ad94ed85b7e36
ospalh <ospalh@gmail.com>
2012-10-31 09:42:11 +0100

    A bit of work on another plugin.

--- Delete unused tags.py
+++ Delete unused tags.py
@@ -3,11 +3,14 @@
 # Based in part on code by Damien Elmes <anki@ichi2.net>
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 
-# import re
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QAction
+
 from anki.hooks import wrap
-from aqt.qt import *
-from aqt import mw
+from anki.lang import _
+# from aqt import mw
 from aqt.browser import Browser
+from aqt.utils import askUser
 
 """Add-on for Anki 2 to remove unused tags."""
 
@@ -15,17 +18,16 @@ __version__ = "0.0.1"
 
 
 def remove_unused_tags():
-    print ('Not removing tags.')
-    pass
+    if askUser(_(u"Remove all unused tags?")):
+        showInfo('Please wait until the add-on is finished.')
 
 
 def add_rm_tags_action(self):
     rm_tags_action = QAction(self)
-    rm_tags_action.setText("Remove unused tags")
+    rm_tags_action.setText("Remove unused tags...")
     self.connect(rm_tags_action, SIGNAL("triggered()"), remove_unused_tags)
     self.form.menuEdit.addSeparator()
     self.form.menuEdit.addAction(rm_tags_action)
-    pass
 
 
 Browser.setupMenus = wrap(Browser.setupMenus, add_rm_tags_action)
