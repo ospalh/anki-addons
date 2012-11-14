ca2942606f74be089e1168abe673d15de6f4dea0
ospalh <ospalh@gmail.com>
2012-11-14 15:27:11 +0100

    Updated manual audio gui a bit.

--- downloadaudio/update_gui.py
+++ downloadaudio/update_gui.py
@@ -9,7 +9,7 @@ Change the download audio parameters on user input.
 """
 
 from PyQt4.QtCore import SIGNAL, SLOT
-from PyQt4.QtGui import QDialog, QDialogButtonBox, QGridLayout, QHBoxLayout, \
+from PyQt4.QtGui import QDialog, QDialogButtonBox, QFrame, QGridLayout, QHBoxLayout, \
     QIcon, QLabel, QLineEdit, QVBoxLayout
 
 from anki.lang import _
@@ -49,20 +49,32 @@ class ReviewFields(QDialog):
 
     def initUI(self):
         language_help = _(u'''<h4>Language code.</h4>
-<p>This will be transmitted as part of the requst sent to Google
-TTS. Use a standard language code here. Using invalid values or codes
-of unsupported languages will result in no downloads. Do <em>not</em>
-use domain codes (E.g. use <code>zh</code> rather than <code>cn</code>
-for Chinese.)</p>''')
+<p>This will be transmitted as part of the requst sent to the
+sites. As some sites only support one language, this is also used to
+decide where to send the requests. Use a standard language code
+here. Using invalid values or codes of unsupported languages will
+result in no downloads. Do <em>not</em> use domain codes (E.g. use
+<code>zh</code> rather than <code>cn</code> for Chinese.)</p>''')
         self.setWindowTitle(_(u'Anki – Download audio'))
         self.setWindowIcon(QIcon(":/icons/anki.png"))
         layout = QVBoxLayout()
         self.setLayout(layout)
-        explanation = QLabel(self)
-        explanation.setText(
-            _(u'Please edit the texts below or change the language.'))
-        layout.addWidget(explanation)
+        edit_text_head = QLabel(_('''\
+<h4>Requests to send to the download sites</h4>
+In split edit fields, set the expression (kanji) on the left, the
+reading (kana) on the right.
+'''))
+        layout.addWidget(edit_text_head)
         self.create_data_rows(layout)
+        line = QFrame(self)
+        line.setFrameShape(QFrame.HLine)
+        line.setFrameShadow(QFrame.Sunken)
+        layout.addWidget(line)
+        lcode_head = QLabel(_('''\
+<h4>Language code</h4>
+Set the language of the request here.
+'''))
+        layout.addWidget(lcode_head)
         lang_hlayout = QHBoxLayout()
         lc_label = QLabel(_(u'Language code:'), self)
         lang_hlayout.addWidget(lc_label)
@@ -86,12 +98,6 @@ for Chinese.)</p>''')
 
     def create_data_rows(self, layout):
         gf_layout = QGridLayout()
-        gtts_head_label = QLabel(_('''\
-<h4>Requests to send to the download sites</h4>
-In split edit fields, set the expression (kanji) on the left, the
-reading (kana) on the right.
-'''))
-        gf_layout.addWidget(gtts_head_label, 0, 0, 1, 3)
         for num, (source, dest, text, base, ruby) \
                 in enumerate(self.data_fields):
             # We create all three QTextEdits for each item and hide
@@ -99,7 +105,7 @@ reading (kana) on the right.
             # empty.
             label = QLabel(u'{0}:'.format(source))
             label.setToolTip(_(u'Source of the request text'))
-            gf_layout.addWidget(label, num + 1, 0)
+            gf_layout.addWidget(label, num, 0)
             ledit = QLineEdit(text)
             self.text_lineedits.append(ledit)
             bedit = QLineEdit(base)
@@ -107,7 +113,7 @@ reading (kana) on the right.
             redit = QLineEdit(ruby)
             self.ruby_lineedits.append(redit)
             if text:
-                gf_layout.addWidget(ledit, num + 1, 1, 1, 2)
+                gf_layout.addWidget(ledit, num, 1, 1, 2)
                 ledit.setToolTip(
                 _(u'''<h4>Text of the request.</h4>
 <p>Edit this as appropriate.  Clear it to not download anything for
@@ -116,11 +122,11 @@ this field.</p>'''))
                 redit.hide()
             else:
                 ledit.hide()
-                gf_layout.addWidget(bedit, num + 1, 1)
+                gf_layout.addWidget(bedit, num, 1)
                 bedit.setToolTip(_(u'''<h4>Kanji of the request.</h4>
 <p>Edit this as appropriate.  Clear it to not download anything for
 this field.  For pure kana words, enter (or keep) the kana here.</p>'''))
-                gf_layout.addWidget(redit, num + 1, 2)
+                gf_layout.addWidget(redit, num, 2)
                 redit.setToolTip(
                 _(u'''<h4>Kana of the request.</h4>
 <p>Edit this as appropriate.  For pure kana words, enter (or keep) the
