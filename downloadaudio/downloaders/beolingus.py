de6004da41cf92d1288363e196fcb2a57acada58
ospalh <ospalh@gmail.com>
2012-11-17 17:40:39 +0100

    Handle cases where we have things like 'sink {verb}' as title of the popup.

--- downloadaudio/downloaders/beolingus.py
+++ downloadaudio/downloaders/beolingus.py
@@ -33,7 +33,10 @@ class BeolingusDownloader(AudioDownloader):
         # Seen this encoding in their page. Oh, my.
         self.site_encoding = 'ISO-8859-1'
         self.speak_code = 'dings.cgi?speak='
-        self.text_code = 'text='
+        # I have seen "text=sink%20{verb}" and the like. The simple text
+        # match wasn't good enough.
+        # self.text_code = 'text='
+        self.text_re = u'text={0}(?:%20{{([a-zA-Z ]+)}})?$'
         self.services_dict = {'de': 'de-en', 'en': 'en-de', 'es': 'es-de'}
         """
         Mapping of languages to "services".
@@ -61,18 +64,30 @@ class BeolingusDownloader(AudioDownloader):
         href_list = self.uniqify_list(href_list)
         href_list = [href for href in href_list
                      if (self.speak_code + self.language) in href]
-        href_list = [href for href in href_list
-                     if href.endswith(self.text_code + word)]
-        if href_list:
+        # Unroll this step, so the adding of the extra element becomes
+        # more readable.
+        speak_list = []
+        for href in href_list:
+            re_found = re.search(self.text_re.format(
+                    re.escape(word)), href)
+            if re_found:
+                # NB: the group(1) may be None.
+                speak_list.append((href, re_found.group(1)))
+        if speak_list:
             # Only get the icon when we (seem to) have a pronunciation
             self.maybe_get_icon()
-        for url_to_get in href_list:
+        for url_to_get, part_of_speech in speak_list:
+            # We may have some extra info: the bit in the {}s.
+            extras = dict(Source="Beolingus")
+            if part_of_speech:
+                # As said above, the bit in the curly braces may not be there.
+                extras['Part of speech'] = part_of_speech
             try:
                 word_path, word_file = self.get_word_file(url_to_get, word)
             except ValueError:
                 continue
             self.downloads_list.append(
-                (word_path, word_file, dict(Source="Beolingus")))
+                (word_path, word_file, extras))
 
     def get_word_file(self, popup_url, word):
         """
