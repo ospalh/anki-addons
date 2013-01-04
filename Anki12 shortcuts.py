d0fd60f2b5794a25ac1e87d950251910c1e04b4c
ospalh <ospalh@gmail.com>
2013-01-04 13:46:57 +0100

    Add 6 as key to replay audio. (Use this on the num pad.)

--- Anki12 shortcuts.py
+++ Anki12 shortcuts.py
@@ -10,14 +10,26 @@ __version__ = "1.1.1"
 from aqt import mw
 from aqt.reviewer import Reviewer
 from aqt.qt import *
+from anki.hooks import wrap
 
 mw.other_deck = QShortcut(QKeySequence("Ctrl+w"), mw)
 mw.other_browse = QShortcut(QKeySequence("Ctrl+f"), mw)
-# F5 is back in the standard install (i think) RAS 2012-10-11
-# mw.replay_audio = QShortcut(QKeySequence("F5"), mw)
 
+
+def replay_6(self, evt):
+    """
+    Use "6" to replay audio.
+
+    Use the 6 key to replay audio. Useful for reviewing with the right
+    hand on the numeric key pad.
+    """
+    key = unicode(evt.text())
+    print ('key: {}'.format(key))
+    if key == "6":
+        self.replayAudio()
+
+
+Reviewer._keyHandler = wrap(Reviewer._keyHandler, replay_6)
 mw.connect(mw.other_deck, SIGNAL("activated()"),
            lambda: mw.moveToState("deckBrowser"))
 mw.connect(mw.other_browse, SIGNAL("activated()"), lambda: mw.onBrowse())
-#mw.connect(mw.replay_audio, SIGNAL("activated()"),
-#           lambda: mw.reviewer.replayAudio())
