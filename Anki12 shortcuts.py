04f5384f8b673237117c37362cb8266325e6826e
ospalh <ospalh@gmail.com>
2013-01-04 13:55:51 +0100

    Clean-up, add module doc-string.

--- Anki12 shortcuts.py
+++ Anki12 shortcuts.py
@@ -3,14 +3,30 @@
 # © changed version: ospalh@gmail.com
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
 #
-# Emulate some Anki 1.2 shortcuts.
 
-__version__ = "1.1.1"
 
+"""
+Emulate some Anki 1.2 shortcuts. Add replay short-cut.
+
+Add-on for Anki2 that adds Ctrl-w to clase the deck and Ctr-f to open
+the card browser. These shortcuts worked in Anki 1.2. This part of the
+add-on is basically identical to the "Accept Anki 1.2 shortcuts to
+list decks, add cards and open browser" add-on, minus the add card.
+
+Also, replay audio when pressing 'i' or '6', outside a text entry
+field.  This can be used to quickly review with either the left hand
+on a Dvorak keyboard (See also my "Dvorak keys" add-on.) or with the
+right hand on the numeric key-pad.
+"""
+
+from PyQt4.QtCore import SIGNAL
+from PyQt4.QtGui import QKeySequence, QShortcut
+
+from anki.hooks import wrap
 from aqt import mw
 from aqt.reviewer import Reviewer
-from aqt.qt import *
-from anki.hooks import wrap
+
+__version__ = "1.1.1"
 
 mw.other_deck = QShortcut(QKeySequence("Ctrl+w"), mw)
 mw.other_browse = QShortcut(QKeySequence("Ctrl+f"), mw)
