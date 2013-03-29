2479c1ba0322f1d0af847c8fa2bd39d91cbdb7b3
ospalh <ospalh@gmail.com>
2013-03-29 12:43:17 +0100

    Reworked a bit. Some clean up of my own code. Some clean up of Yichao Zhou contribution. (I just like rolling the code out a bit. There is nothing inherently wrong with using something like "close_fds=not isWin" as an argument to a functio. I just think my way looks better.) Also try to look for the mplayer the binary install brings along. Untested.

--- Quick replay.py
+++ Quick replay.py
@@ -1,7 +1,9 @@
 # -*- mode: Python ; coding: utf-8 -*-
-# Copyright: Roland Sieker ( ospalh@gmail.com )
+# © 2012–3: Roland Sieker <ospalh@gmail.com>
+# Bits contributed by Yichao Zhou
 # License: GNU GPL, version 3 or later;
 # http://www.gnu.org/copyleft/gpl.html
+
 """
 Anki 2 add-on that changes the way sound and videos are played.
 """
@@ -10,21 +12,25 @@ import copy
 import os
 import re
 import subprocess
-import sys
 
 from anki.sound import playFromText, play
-from aqt import utils, reviewer
+from anki.utils import isWin
+from aqt import moduleDir, utils, reviewer
 
 __version__ = "1.1.1"
+
+if isWin:
+    # Looks like we should use these to hide the CMD/shell
+    # window. From a patch by Yichao Zhou. Thanks.
+    subprocess.STARTUPINFO.wShowWindow = subprocess.SW_HIDE
+    subprocess.STARTUPINFO.dwFlags = subprocess.STARTF_USESHOWWINDOW
 
 sound_re = '\[sound:(.*?)\]'
 
 command_list = ['mplayer', '-really-quiet']
-
-is_win32 = sys.platform.startswith("win32")
-
-if is_win32:
+if isWin:
     command_list += ['-ao', 'win32']
+
 
 def patched_play_from_text(text):
     matches = re.findall(sound_re, text)
@@ -42,29 +48,38 @@ def patched_play_from_text(text):
 
 
 def play_with_mplayer(files):
-    # We don't do the file name fixing. The point of this is to play
-    # it quickly. When there is no easy way to do it on Windows, than
-    # remove this add-on. So, just throw the names at mplayer.
+    """
+    Play files with mplayer.
 
-    if is_win32:
-	subprocess.STARTUPINFO.wShowWindow = subprocess.SW_HIDE
-	subprocess.STARTUPINFO.dwFlags = subprocess.STARTF_USESHOWWINDOW
-
-    tmp_play_list = copy.copy(command_list)
-    tmp_play_list.extend(files)
-    try:
-        subprocess.Popen(tmp_play_list,
-                         shell=False, stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         close_fds=not is_win32)
-    except OSError:
-        # On Macs, we get ‘Interruppted system call’s. Just
-        # ignore, like anki’s sound module does.
-        pass
+    This is the piece-de-resistance of the add-on. We take the file
+    names in files an throw them at mplayer, without using the audio
+    queue. We don't do the file name fixing, either.. The point of
+    this is to play it quickly. There have been a few additions so
+    presumably it works on windows now.
+    """
+    tmp_play_list = copy.copy(command_list) + files
+    if isWin:
+        subprocess.Popen(
+            tmp_play_list, shell=False, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=False)
+    else:
+        try:
+            subprocess.Popen(
+                tmp_play_list, shell=False, stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+        except OSError:
+            # On Macs, we get ‘Interruppted system call’s. Just
+            # ignore, like anki’s sound module does.
+            pass
 
 
 def which(program):
-    """Return path of command."""
+    """
+    Return path of command.
+
+    Return path of command program. We add the path of Anki because
+    the binary install brings along an mplayer of its own.
+    """
     def is_exe(fpath):
         return os.path.exists(fpath) and os.access(fpath, os.X_OK)
 
@@ -73,7 +88,7 @@ def which(program):
         if is_exe(program):
             return program
     else:
-        for path in os.environ["PATH"].split(os.pathsep):
+        for path in os.environ["PATH"].split(os.pathsep) + [moduleDir, ]:
             exe_file = os.path.join(path, program)
             if is_exe(exe_file):
                 return exe_file
@@ -81,19 +96,20 @@ def which(program):
 
 
 def find_mplayer():
-    """ Try to find mplayer
+    """
+    Try to find mplayer
 
     Look if we have mplayer. Fix it by adding an .exe on windows first. Clear
     the commands when we don't have one.
     """
     global command_list
     if command_list:
-	if is_win32:
+        if isWin:
             command_list[0] += '.exe'
         if not which(command_list[0]):
             # Complain,
-	    warn_string = u'''Replay with mplayer add-on: Could not find {0} \
-		    in path. Please download and install it.'''
+            warn_string = u'''Quick replay add-on: Could not find {0} \
+                   in path. Please download and install it.'''
             utils.showWarning(warn_string.format(command_list[0]))
             # and clear the list
             command_list = None
@@ -109,4 +125,3 @@ playFromText = patched_play_from_text
 reviewer.playFromText = patched_play_from_text
 
 find_mplayer()
-
