45914ca74610452e3e03567afc82a449d4947cd6
ospalh <ospalh@gmail.com>
2013-04-03 10:49:06 +0200

    A first version of code to show the tool tips. Looks ugly. Needs tweaking.

---
+++ kanjitips/show_tips.js
@@ -0,0 +1,16 @@
+$(function() {
+    $( document ).tooltip({
+        position: {
+            my: "center bottom-20",
+            at: "center top",
+            using: function( position, feedback ) {
+                $( this ).css( position );
+                $( "<div>" )
+                    .addClass( "arrow" )
+                    .addClass( feedback.vertical )
+                    .addClass( feedback.horizontal )
+                    .appendTo( this );
+            }
+        }
+    });
+});
