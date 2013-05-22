e63ef532024a01a7644780a7572bcb52c89bf8e2
ospalh <ospalh@gmail.com>
2013-05-22 17:04:47 +0200

    Clean up.

--- compare_to_kana.py
+++ compare_to_kana.py
@@ -32,16 +32,12 @@ def correct_kana(res, typed, right, card):
     try:
         fld = re.search(r'\[\[type:([^\]]+)\]\]', card.a()).group(1)
     except AttributeError:
-        print (u'no type in {}'.format(card.a()))
         # No typed answer to show at all.
         return res
     if not reading_field in fld.lower() or fld.startswith("cq:"):
-        print 'cloze  or not reading'
         return res
     if not japanese_model in card.model()[u'name'].lower():
-        print 'not japanese'
         return res
-    print (u'do the compare with {}, not with {}'.format(kana(right), right))
     return mw.reviewer.correct(u'', typed, kana(right), card, showBad=True)
 
 
