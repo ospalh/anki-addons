b654464627bb7119d133c247c0135e5a723e1d67
ospalh <ospalh@gmail.com>
2013-06-10 12:42:03 +0200

    Revert "remove the kanji digrams. too much hassle on this branch"
    This reverts commit e1f47ad4a28b1c915a32a83733128cbe8cbf8236.

---
+++ stroke-order-kanji/_kanji_script.js
@@ -0,0 +1,17 @@
+//<![CDATA[
+/*
+  This is part of kanji-colorize, which shows KanjiVG data as colored
+  stroke order diagrams.
+
+  This is just a dummy file.
+
+  This file copyright © 2012 Roland Sieker <ospalh@gmail.com>
+  License: GNU AGPL, version 3 or later;
+  http://www.gnu.org/licenses/agpl.html
+
+*/
+
+function init(evt) {
+}
+
+//]]>
