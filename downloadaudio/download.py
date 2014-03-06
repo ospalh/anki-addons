181b2a515a6bc1db2ca35d1b78095944fea01b79
Harsh Singh <hisingh1@gmail.com>
2014-03-06 08:03:51 -0600

    Update download.py
    adds ' as global keybinding to add audio to note

--- downloadaudio/download.py
+++ downloadaudio/download.py
@@ -35,7 +35,7 @@ manual.
 """
 
 import os
-from PyQt4.QtGui import QAction, QIcon, QMenu
+from PyQt4.QtGui import QAction, QIcon, QMenu, QKeySequence, QShortcut
 from PyQt4.QtCore import SIGNAL
 
 from aqt import mw
@@ -238,6 +238,7 @@ mw.note_download_action.setIcon(QIcon(os.path.join(icons_dir,
 mw.note_download_action.setToolTip(
     "Download audio for all audio fields of this note.")
 mw.connect(mw.note_download_action, SIGNAL("triggered()"), download_for_note)
+mw.note_download_signal = QShortcut(QKeySequence("'"), mw)
 
 mw.side_download_action = QAction(mw)
 mw.side_download_action.setText(u"Side audio")
