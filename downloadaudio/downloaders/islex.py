b7d26bee9836382c12b9f6162d75c706a8739383
Daniel Eriksson <daniel@deriksson.se>
2015-11-05 10:12:17 -0800

    Add downloader module islex.py

---
+++ downloadaudio/downloaders/islex.py
@@ -0,0 +1,13 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright © 2015 Daniel Eriksson <daniel@deriksson.se>
+#
+# License: GNU AGPL, version 3 or later;
+# http://www.gnu.org/copyleft/agpl.html
+
+
+'''
+Download pronunciations for Icelandic from islex.is
+'''
+
+
