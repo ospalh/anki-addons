76f76d94e7e17fc18176efe313c5df3de98a670b
Daniel Eriksson <daniel@deriksson.se>
2015-11-05 13:07:06 -0800

    Create download_files method
    This code makes a search on Islex and gets the soup
    for further processing.

--- downloadaudio/downloaders/islex.py
+++ downloadaudio/downloaders/islex.py
@@ -10,6 +10,7 @@
 Download pronunciations for Icelandic from islex.is
 '''
 
+import urllib
 
 from downloader import AudioDownloader # Change to .downloader when testing in Anki
 
@@ -20,3 +21,20 @@ class IslexDownloader(AudioDownloader):
     def __init__(self):
         AudioDownloader.__init__(self)
         self.url = 'http://islex.is/'
+
+    def download_files(self, field_data):
+        self.downloads_list = []
+        # Don't ask what the flags do. Got them by sniffing the POST request
+        # sent by the advanced search on Islex with the desired settings.
+        if not self.language.lower().startswith('is'):
+            return
+
+        qdict = {'finna': 1,
+                 'dict': 'SE',
+                 'erflokin': 1,
+                 'nlo': 1,
+                 'fuzz': 1,
+                 'samleit': field_data}
+
+        # Get soup from search
+        soup = self.get_soup_from_url(self.url + '?' + urllib.urlencode(qdict))
