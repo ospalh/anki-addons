5232aa8948a9bb895baa2cbca434bbea4001ab76
ospalh <ospalh@gmail.com>
2015-11-06 11:00:30 +0100

    Clean up. A bit more EAFP. A bitmore Pythonic (“if list”, then maybe iterate over empty list) Fewer comments in the file. (But thanks for the long commit message.)

--- downloadaudio/downloaders/islex.py
+++ downloadaudio/downloaders/islex.py
@@ -1,14 +1,15 @@
 # -*- mode: python; coding: utf-8 -*-
 #
 # Copyright © 2015 Daniel Eriksson <daniel@deriksson.se>
+# Copyright © 2015 Roland Sieker <ospalh@gmail.com>
 #
 # License: GNU AGPL, version 3 or later;
 # http://www.gnu.org/copyleft/agpl.html
 
 
-'''
+"""
 Download pronunciations for Icelandic from islex.is
-'''
+"""
 
 import urllib
 
@@ -27,62 +28,40 @@ class IslexDownloader(AudioDownloader):
 
     def download_files(self, field_data):
         self.downloads_list = []
-
         if not self.language.lower().startswith('is'):
             return
         if field_data.split:
             return
         if not field_data.word:
             return
-
-        # Don't ask what the flags do. Got them by sniffing the POST request
-        # sent by the advanced search on Islex with the desired settings.
-        qdict = {'finna': 1,
-                 'dict': 'SE',
-                 'erflokin': 1,
-                 'nlo': 1,
-                 'fuzz': 1,
+        # These flags were used by an advanced search on Islex. They
+        # work, but not all of them may be needed.
+        qdict = {'finna': 1, 'dict': 'SE', 'erflokin': 1, 'nlo': 1, 'fuzz': 1,
                  'samleit': field_data.word.encode('utf-8')}
-
-        # Get soup from search
         soup = self.get_soup_from_url(self.url + '?' + urllib.urlencode(qdict))
-
-        # There are a number of cases here:
-        #  0. No match. We get a result summary page telling us we have no matches.
-        #  1. Only one match. We get the dictionary page for the word.
-        #  2. More than one match. We get a result summary page with links to dictionary pages
-        #
-        # For case 2 the results are divided among match on main word and match on inflected form.
-        # Probably best to drop the inflected forms if there are matches on the main word.
-
         href_list = []
-        # check for case 1: Is there a table tag with id="ord"?
         if soup.findAll(attrs=dict(id='ord')):
+            # When we have a table tag with id="ord" we (probably)
+            # have just one word. Use that.
             # Both mp3 and ogg are available. They contain the same sound
             # This will take whichever comes first
-            href_list.append(self.url + soup.find('audio').find('source')['src'])
+            href_list.append(
+                self.url + soup.find('audio').find('source')['src'])
         else:
-            # case 0 or 2
-            try:
-                links = soup.find(attrs={'class': 'leitres'}).find('ul').findAll('a')
-            except AttributeError:
-                # Raised if no results (case 0)
-                return
+            # More than one word. Or 0 words.
+            links = soup.find(
+                attrs={'class': 'leitres'}).find('ul').findAll('a')
+            # When we have 0 words this raises some exception. Which is fine.
             for a in links:
                 try:
                     word_soup = self.get_soup_from_url(self.url + a['href'])
                 except (AttributeError, KeyError):  # What else could go wrong?
                     continue
-                href_list.append(self.url + word_soup.find('audio').find('source')['src'])
-
-        if not href_list:
-            return
-
-        self.maybe_get_icon()
-
+                href_list.append(
+                    self.url + word_soup.find('audio').find('source')['src'])
+        if href_list:
+            self.maybe_get_icon()
         for url in href_list:
-            extras = {'Source': 'Islex'}
-            word_path = self.get_tempfile_from_url(url)
-            self.downloads_list.append(
-                DownloadEntry(
-                    field_data, word_path, extras, self.site_icon))
+            self.downloads_list.append(DownloadEntry(
+                field_data, self.get_tempfile_from_url(url),
+                dict(Source='Islex'), self.site_icon))
