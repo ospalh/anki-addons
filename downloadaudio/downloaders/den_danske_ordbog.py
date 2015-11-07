4980de1402055efe67aa493862f96252dc975b22
Daniel Eriksson <daniel@deriksson.se>
2015-11-06 18:42:03 -0800

    Add frontmatter and subclass AudioDownloader

---
+++ downloadaudio/downloaders/den_danske_ordbog.py
@@ -0,0 +1,25 @@
+# -*- mode: python; coding: utf-8 -*-
+#
+# Copyright © 2015 Daniel Eriksson <daniel@deriksson.se>
+# Copyright © 2015 Roland Sieker <ospalh@gmail.com>
+#
+# License: GNU AGPL, version 3 or later;
+# http://www.gnu.org/copyleft/agpl.html
+
+
+"""
+Download pronunciations for Danish from Den Danske Ordbog
+"""
+
+import urllib
+from .downloader import AudioDownloader
+from ..download_entry import DownloadEntry
+
+
+class DenDanskeOrdbogDownloader(AudioDownloader):
+    """Download audio from Den Danske Ordbog"""
+
+    def __init__(self):
+        AudioDownloader.__init__(self)
+        self.url = 'http://ordnet.dk/ddo/ordbog?'
+        self.icon_url = 'http://ordnet.dk/'
