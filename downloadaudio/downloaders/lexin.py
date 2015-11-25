9dea746aa71304f93df4993e4654376fd0f6be27
Daniel Eriksson <daniel@deriksson.se>
2015-11-24 21:00:28 -0800

    Add code for making request for a word to self.url
    Lexin uses GWT-RPC for communicating with the server. The response is
    parsed and presented using javascript. The request structured was
    uncovered by inspecting the requests sent by the browser. The minimum
    headers were identified using trial and error. The payload could
    potentially be shortened if anyone feels inclined to read the gwt docs.

--- downloadaudio/downloaders/lexin.py
+++ downloadaudio/downloaders/lexin.py
@@ -13,6 +13,7 @@ Download pronunciations from Lexin.
 """
 
 import unicodedata
+import urllib2
 
 from .downloader import AudioDownloader
 from ..download_entry import DownloadEntry
@@ -60,6 +61,25 @@ class LexinDownloader(AudioDownloader):
             return
         self.maybe_get_icon()
         self.download_v1(field_data)
+        # These headers are necessary
+        # Without them, the server returns 500 response
+        headers = {
+            'Content-Type': 'text/x-gwt-rpc; charset=utf-8',
+            'X-GWT-Permutation': 'B3637EFE47DBD18879B4B4582D033CEA'}
+        # GWT-RPC encoded payload string
+        payload = (
+            '7|0|7|http://lexin.nada.kth.se/lexin/lexin/|'
+            'FCDCCA88916BAACF8B03FB48D294BA89|'
+            'se.jojoman.lexin.lexingwt.client.LookUpService|'
+            'lookUpWord|se.jojoman.lexin.lexingwt.client.LookUpRequest/682723451|swe_swe|' +
+            field_data.word.encode('utf-8') + '|1|2|3|4|1|5|5|1|6|1|7|')
+        request = urllib2.Request(
+            self.url,
+            payload, headers)
+        try:
+            response = urllib2.urlopen(request)
+        except:
+            pass
 
     def download_v1(self, field_data):
         """Get pronunciations of a word in Swedish from Lexin
