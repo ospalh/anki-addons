2db173985808e452fbcb63427d63cc726bcffa65
ospalh <ospalh@gmail.com>
2016-04-04 10:01:56 +0200

    Use my version of mhwave for everybody. (Check the feature-ospalh-special branch for a bit more history)

--- mhwave.py
+++ mhwave.py
@@ -1,9 +1,9 @@
 # -*- mode: Python ; coding: utf-8 -*-
-# © 2012–3: Roland Sieker <ospalh@gmail.com>
-# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-"""
-Anki 2 add-on that opens sweep to edit the audio files.
-"""
+# © 2012–4: Roland Sieker <ospalh@gmail.com>
+#
+# License: GNU GPL, version 3 or later;
+# http://www.gnu.org/copyleft/gpl.html
+u"""Anki 2 add-on that opens an audio editor."""
 
 import copy
 import os
@@ -13,14 +13,14 @@ import sys
 
 from aqt import utils
 from aqt import mw
-from PyQt4.QtGui import QAction
+from PyQt4.QtGui import QAction, QIcon, QMenu
 from PyQt4.QtCore import SIGNAL
 
-__version__ = "1.0.1"
+__version__ = "2.0.0"
 
 sound_re = ur'\[sound:(.*?)\]'
 
-command_list = ['sweep-audio-editor']
+command_list = ['mhwaveedit']
 sound_ending_list = ['.mp3', '.wav', '.flac', '.ogg']
 
 
@@ -37,31 +37,26 @@ def sound_ending(fname):
     return None
 
 
-def sweep_files(note):
-    """
-    Call sweep with all sounds from the note.
-    """
+def edit_files(note):
+    u"""Call the audio editor with all sounds from the note"""
     # First, join all fields. Use some random field delimiter. (Could
     # be '', i guess.) EAFP, raise stuff when we don't have a note.
     text = '@'.join(note.fields)
     matches = [fn for fn in re.findall(sound_re, text) if sound_ending(fn)]
     if command_list and matches:
-        call_sweep(matches)
+        call_edit(matches)
 
 
-def call_sweep(files):
-    u"""Start the programme sweep to edit files."""
+def call_edit(files):
+    u"""Start the audio editor to edit files."""
     # We don't do the file name fixing. The point of this is to edit
-    # the files in place. I don't think there is a sweep version for
-    # windows. Maybe for cygwin. As this is a quick hack for me, i
-    # don't really mind.
-    tmp_sweep_list = copy.copy(command_list)
-    tmp_sweep_list.extend(files)
+    # the files in place.
+    tmp_edit_list = copy.copy(command_list)
+    tmp_edit_list.extend(files)
     try:
-        subprocess.Popen(tmp_sweep_list,
-                         shell=False, stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                         close_fds=True)
+        subprocess.Popen(
+            tmp_edit_list, shell=False, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
     except OSError:
         # On Macs, we get ‘Interruppted system call’s. Just
         # ignore, like anki’s sound module does.
@@ -86,11 +81,10 @@ def which(program):
     return None
 
 
-def find_sweep():
-    """
-    Try to find sweep
+def find_editor():
+    u"""Try to find the audio editor
 
-    Look if we have sweep. Fix it by adding an .exe on
+    Look if we have the audio ediotr. Fix it by adding an .exe on
     windows first. Clear the commands when we don't have one.
     """
     global command_list
@@ -99,7 +93,7 @@ def find_sweep():
             command_list[0] += '.exe'
         if not which(command_list[0]):
             # Complain,
-            warn_string = u'''Replay with sweep add-on: Could not find {0} \
+            warn_string = u'''Audio editor add-on: Could not find {0} \
 in path. Please download and install it.'''
             utils.showWarning(warn_string.format(command_list[0]))
             # and clear the list
@@ -107,23 +101,40 @@ in path. Please download and install it.'''
     return command_list
 
 
-def sweep_current_note():
+def edit_current_note():
     u"""Call the action to edit the sound files of the current note."""
     try:
-        sweep_files(mw.reviewer.card.note())
+        edit_files(mw.reviewer.card.note())
     except AttributeError:
         # No note.
         pass
 
 
-if find_sweep():
-    # Now add to the menu
-    mw.sweep_audio_fiels_action = QAction(mw)
-    mw.sweep_audio_fiels_action.setText(u"Edit audio")
-    # mw.sweep_audio_fiels_action.setIcon(
-    #     QIcon(os.path.join(icons_dir, 'download_audio_manual.png')))
-    mw.sweep_audio_fiels_action.setToolTip(
-        "Edit audio files of the current note with sweep-audio-editor.")
-    mw.connect(mw.sweep_audio_fiels_action, SIGNAL("triggered()"),
-               sweep_current_note)
-    mw.form.menuTools.addAction(mw.sweep_audio_fiels_action)
+if find_editor():
+    # Either reuse an edit-media sub-menu created by another add-on
+    # (probably by Y.T., notably the download audio add-on) or create
+    # that menu. When we already have that menu, add a separator,
+    # otherwise create that menu.
+    try:
+        mw.edit_media_submenu.addSeparator()
+    except AttributeError:
+        mw.edit_media_submenu = QMenu(u"&Media", mw)
+        mw.form.menuEdit.addSeparator()
+        mw.form.menuEdit.addMenu(mw.edit_media_submenu)
+    # Now add to that menu
+    mw.edit_audio_fiels_action = QAction(mw)
+    mw.edit_audio_fiels_action.setText(u"Edit audio")
+    icons_dir = os.path.join(mw.pm.addonFolder(), 'color_icons')
+    try:
+        # Bad hack. Use the icon brought along from another add-on and
+        # nicked from the program we use. That program is GPLed, so we
+        # should be OK with that.
+        mw.edit_audio_fiels_action.setIcon(
+            QIcon(os.path.join(icons_dir, 'mhwaveedit.png')))
+    except:
+        pass
+    mw.edit_audio_fiels_action.setToolTip(
+        "Edit audio files of the current note with mhwave.")
+    mw.connect(mw.edit_audio_fiels_action, SIGNAL("triggered()"),
+               edit_current_note)
+    mw.edit_media_submenu.addAction(mw.edit_audio_fiels_action)
