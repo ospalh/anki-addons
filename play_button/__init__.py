43ea4dd6b49abaa6f8c68459f2a1a5ec8a8cba42
ospalh <ospalh@gmail.com>
2017-10-24 15:43:21 +0200

    Showing the SVG play button works once more. Playing sounds to come.

--- play_button/__init__.py
+++ play_button/__init__.py
@@ -1,19 +1,22 @@
 # -*- mode: Python ; coding: utf-8 -*-
 #
-# Copyright © 2013–16 Roland Sieker <ospalh@gmail.com>
+# Copyright © 2013–17 Roland Sieker <ospalh@gmail.com>
 #
 # License: GNU AGPL, version 3 or later;
 # http://www.gnu.org/copyleft/agpl.html
 
 """Add-on for Anki 2 to add AnkiDroid-style replay buttons."""
 
+import html.parser
 import os
 import re
 import shutil
+import urllib.parse
 
 from bs4 import BeautifulSoup
 from PyQt5.QtCore import QUrl
 from PyQt5.QtGui import QDesktopServices
+
 
 from anki.cards import Card
 from anki.hooks import addHook, wrap
@@ -33,7 +36,7 @@ hide_class_name = u'browserhide'
 
 def svg_css(Card):
     """Add the svg button style to the card style"""
-    return u"""<style scoped>
+    return """<style scoped>
 .replaybutton span {
   display: inline-block;
   vertical-align: middle;
@@ -55,33 +58,32 @@ def svg_css(Card):
 
 def play_button_filter(
         qa_html, qa_type, dummy_fields, dummy_model, dummy_data, dummy_col):
-    u"""
+    """
     Filter the questions and answers to add play buttons.
     """
 
     def add_button(sound):
-        u"""
-        Add a button after the match.
+        """Add a button after the match.
 
         Add a button after the match to replay the audio. The title is
         set to "Replay" on the question side to hide information or to
         the file name on the answer.
-        
-        The filename is first unescaped and then percentage encoded. The
-        utf8 encoding is a workaround for unicode support. In python 3:
-        
-        from urllib.parse import quote
-        clean_fn = quote(unescaped_fn)
+
+        The filename is first unescaped and then percentage
+        encoded. For example, an image of a German rock album may go
+        from “&Ouml;.jpg” to “Ö.jpg” to “%C3%96.jpg”.
+
         """
         fn = sound.group(1)
-        unescaped_fn = BeautifulStoneSoup(fn,
-            convertEntities=BeautifulStoneSoup.HTML_ENTITIES).contents[0]
-        clean_fn = quote(unescaped_fn.encode('utf8'))
+        # Cut out some escaping of HTML enteties that is done
+        # automatically by BS4
+        unescaped_fn = html.parser.HTMLParser().unescape(fn)
+        clean_fn = urllib.parse.quote(unescaped_fn)
         if 'q' == qa_type:
-            title = u"Replay"
+            title = "Replay"
         else:
             title = fn
-        return u"""{orig}<a href='javascript:py.link("ankiplay{link_fn}");' \
+        return """{orig}<a href='javascript:py.link("ankiplay{link_fn}");' \
 title="{ttl}" class="replaybutton browserhide"><span><svg viewBox="0 0 32 32">\
 <polygon points="11,25 25,16 11,7"/>Replay</svg></span></a>\
 <span style="display: none;">&#91;sound:{fn}&#93;</span>""".format(
@@ -94,7 +96,7 @@ title="{ttl}" class="replaybutton browserhide"><span><svg viewBox="0 0 32 32">\
 
 
 def review_link_handler_wrapper(reviewer, url):
-    u"""Play the sound or call the original link handler."""
+    """Play the sound or call the original link handler."""
     if url.startswith("ankiplay"):
         play(url[8:])
     else:
@@ -102,7 +104,7 @@ def review_link_handler_wrapper(reviewer, url):
 
 
 def simple_link_handler(url):
-    u"""Play the file."""
+    """Play the file."""
     if url.startswith("ankiplay"):
         play(url[8:])
     else:
@@ -110,18 +112,18 @@ def simple_link_handler(url):
 
 
 def add_clayout_link_handler(clayout, dummy_t):
-    u"""Make sure we play the files from the card layout window."""
+    """Make sure we play the files from the card layout window."""
     clayout.forms[-1]['pform'].frontWeb.setLinkHandler(simple_link_handler)
     clayout.forms[-1]['pform'].backWeb.setLinkHandler(simple_link_handler)
 
 
 def add_preview_link_handler(browser):
-    u"""Make sure we play the files from the preview window."""
+    """Make sure we play the files from the preview window."""
     browser._previewWeb.setLinkHandler(simple_link_handler)
 
 
 def reduce_format_qa(self, text):
-    u"""Remove elements with a given class before displaying."""
+    """Remove elements with a given class before displaying."""
     soup = BeautifulSoup(text, 'html.parser')
     for hide in soup.findAll(True, {'class': re.compile(
             '\\b' + hide_class_name + '\\b')}):
@@ -132,12 +134,15 @@ def reduce_format_qa(self, text):
 original_review_link_handler = Reviewer._linkHandler
 Reviewer._linkHandler = review_link_handler_wrapper
 
-original_format_qa = DataModel.formatQA
-DataModel.formatQA = reduce_format_qa
+# TODO: hide stuff from the browser again
+#original_format_qa = DataModel.formatQA
+#DataModel.formatQA = reduce_format_qa
 
 old_css = Card.css
 Card.css = svg_css
 
 addHook("mungeQA", play_button_filter)
 Browser._openPreview = wrap(Browser._openPreview, add_preview_link_handler)
-CardLayout.addTab = wrap(CardLayout.addTab, add_clayout_link_handler)
+
+# TODO: fix this
+#CardLayout.addTab = wrap(CardLayout.addTab, add_clayout_link_handler)
